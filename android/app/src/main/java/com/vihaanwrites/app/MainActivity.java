70683f91b1ff1f2fa507433247b18a06f1a10a38
Abhishek Shrivastava <abhishekkumardipu@gmail.com>
2026-05-08 02:44:48 +0530

    Issue resolved

--- android/app/src/main/java/com/vihaanwrites/app/MainActivity.java
+++ android/app/src/main/java/com/vihaanwrites/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.vihaanwrites.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- android/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.vihaan.app',
-  appName: 'Vihaan Writes',
-  webDir: 'www'
-};
-
-export default config;
