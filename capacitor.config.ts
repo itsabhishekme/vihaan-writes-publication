82308cb849d50d72153cccd92820c73f2d209734
Abhishek Shrivastava <abhishekkumardipu@gmail.com>
2026-05-07 20:25:22 +0530

    Build Fixed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,13 @@
-/// <reference types="@capacitor/cli" />
-
-import type { CapacitorConfig } from "@capacitor/cli";
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: "com.vihaanwrites.app",
-  appName: "Vihaan Writes",
-  webDir: "out",
+  appId: 'com.vihaanwrites.app',
+  appName: 'Vihaan Writes',
+  webDir: 'out',
+
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
