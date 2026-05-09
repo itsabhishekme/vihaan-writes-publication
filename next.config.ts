af43023b1bc748db620658c2e2a944ba061d08e4
Abhishek Shrivastava <abhishekkumardipu@gmail.com>
2026-05-10 00:08:43 +0530

    Android Build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
 
-const nextConfig: NextConfig = {
-  output: "export",
-};
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
