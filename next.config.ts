1c6f4809a9123be2a637815c365d87b084cbd0d5
Abhishek Shrivastava <abhishekkumardipu@gmail.com>
2026-05-09 14:55:10 +0530

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compress: true,
-  poweredByHeader: false,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
