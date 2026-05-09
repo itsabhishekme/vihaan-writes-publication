dfcd25e5141c0e41c8a623cb5f2bd0dc97dcc0f8
Abhishek Shrivastava <abhishekkumardipu@gmail.com>
2026-05-09 13:18:12 +0530

    Update log

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compress: true,
+  poweredByHeader: false,
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
